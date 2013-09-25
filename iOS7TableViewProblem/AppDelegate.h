c444611230fb49ef5da075c31e6cf96d79fed7ae
Matt Glover <matt@duchysoftware.com>
2013-09-25 15:07:44 +0100

    Initial Commit

---
+++ iOS7TableViewProblem/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS7TableViewProblem
+//
+//  Created by Matt Glover on 25/09/2013.
+//  Copyright (c) 2013 Duchy Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS7TableViewProblem/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOS7TableViewProblem
+//
+//  Created by Matt Glover on 25/09/2013.
+//  Copyright (c) 2013 Duchy Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
